41334fc29184ccaf3803b456d608de1161eec160
Mikel Giffin <giffinmike@hotmail.com>
2024-05-17 18:47:06 -0700

    cleaned up errors

--- backend/config.ts
+++ backend/config.ts
@@ -1,4 +1,3 @@
-// backend/config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
@@ -8,9 +7,9 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
     console.log("MongoDB connected");
-  } catch (err) {
+  } catch (err: any) {
     console.error(err.message);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
