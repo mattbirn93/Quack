f93d1b0f8ce93288e3cdffb96cdc944355a5c368
Mikel Giffin <giffinmike@hotmail.com>
2024-05-21 13:42:04 -0700

    test1

--- app/(tabs)/Script/index.tsx
+++ app/(tabs)/Script/index.tsx
@@ -1,24 +1,25 @@
-import React, { useRef, useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  ActivityIndicator,
-  Button,
-} from 'react-native';
-import { RichEditor, RichToolbar } from 'react-native-pell-rich-editor';
-import axios from 'axios';
-import io from 'socket.io-client';
+// import React, { useRef, useEffect, useState } from 'react';
+// import {
+//   View,
+//   Text,
+//   FlatList,
+//   StyleSheet,
+//   ActivityIndicator,
+//   Button,
+// } from 'react-native';
+// import { RichEditor, RichToolbar } from 'react-native-pell-rich-editor';
+// import axios from 'axios';
+// import io from 'socket.io-client';
+// import { yText } from '../../../frontend/src/config/yjsSetup';
 
-type ContentItem = {
-  type: 'scene-heading' | 'action' | 'character' | 'dialogue';
-  text: string;
-};
+// type ContentItem = {
+//   type: 'scene-heading' | 'action' | 'character' | 'dialogue';
+//   text: string;
+// };
 
-type ScreenwritingEditorProps = {
-  initialContent?: ContentItem[];
-};
+// type ScreenwritingEditorProps = {
+//   initialContent?: ContentItem[];
+// };
 
 // const ScreenwritingEditor: React.FC<ScreenwritingEditorProps> = ({
 //   initialContent = [],
@@ -160,6 +161,29 @@ type ScreenwritingEditorProps = {
 
 /////////////////////////////////////////////////
 
+import React, { useRef, useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Button,
+} from 'react-native';
+import { RichEditor, RichToolbar } from 'react-native-pell-rich-editor';
+import axios from 'axios';
+import io from 'socket.io-client';
+import { yText } from '../../../frontend/src/config/yjsSetup';
+
+type ContentItem = {
+  type: 'scene-heading' | 'action' | 'character' | 'dialogue';
+  text: string;
+};
+
+type ScreenwritingEditorProps = {
+  initialContent?: ContentItem[];
+};
+
 const ScreenwritingEditor: React.FC<ScreenwritingEditorProps> = ({
   initialContent = [],
 }) => {
@@ -170,10 +194,11 @@ const ScreenwritingEditor: React.FC<ScreenwritingEditorProps> = ({
       try {
         const response = await axios.post(
           'http://192.168.0.211:5001/api/users',
+          // 'http://localhost:5001/api/users',
           {
-            first_name: 'From front end UseEffect2',
-            last_name: 'aFrom front end UseEffect2',
-            email: 'From front end UseEffect2@gmail.com',
+            first_name: 'From front end UseEffect7',
+            last_name: 'From front end UseEffect7',
+            email: 'From front end UseEffect7@gmail.com',
             scripts_id_array: [],
           },
         );
@@ -208,9 +233,9 @@ const ScreenwritingEditor: React.FC<ScreenwritingEditorProps> = ({
   // Here is our version of a simple request to add a user to the MONGOLITIS DATABSE YOOOOOOOOOOOOOOOO!!!!!!
   const addUser = () => {
     const data = {
-      first_name: 'From front end websockets version2',
-      last_name: 'From front end websockets version2',
-      email: 'From front end websockets version2@gmail.com',
+      first_name: 'From front end websockets version6',
+      last_name: 'From front end websockets version6',
+      email: 'From front end websockets version6@gmail.com',
       scripts_id_array: [],
     };
     socket.emit('add_user', data);
@@ -258,3 +283,149 @@ const styles = StyleSheet.create({
 });
 
 export default ScreenwritingEditor;
+
+///////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////
+
+// import React, { useRef, useEffect, useState } from 'react';
+// import { View, Text, StyleSheet, Button } from 'react-native';
+// import { RichEditor, RichToolbar } from 'react-native-pell-rich-editor';
+// import axios from 'axios';
+// import io from 'socket.io-client';
+// import { yText } from '../../../frontend/src/config/yjsSetup';
+
+// type ContentItem = {
+//   type: 'scene-heading' | 'action' | 'character' | 'dialogue';
+//   text: string;
+// };
+
+// type ScreenwritingEditorProps = {
+//   initialContent?: ContentItem[];
+// };
+
+// const ScreenwritingEditor: React.FC<ScreenwritingEditorProps> = ({
+//   initialContent = [],
+// }) => {
+//   const editorRef = useRef<RichEditor>(null);
+
+//   // REST API CRUD post request for creating a user named Michael Filoramo
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       console.log('BEFORE');
+//       try {
+//         const response = await axios.post(
+//           'http://192.168.0.211:5001/api/users',
+//           {
+//             first_name: 'From front end UseEffect2',
+//             last_name: 'aFrom front end UseEffect2',
+//             email: 'From front end UseEffect2@gmail.com',
+//             scripts_id_array: [],
+//           },
+//         );
+//         console.log('SUCCESS');
+//         console.log('User added:', response.data);
+//       } catch (error) {
+//         console.error('Error adding user:', error);
+//         console.log('FAILED');
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   // WebSocket connection setup
+//   const socket = io('http://192.168.0.211:5001');
+
+//   socket.on('connect', () => {
+//     console.log('Socket connected');
+//   });
+
+//   socket.on('user_added', (response) => {
+//     console.log('SUCCESS');
+//     console.log('User added:', response);
+//   });
+
+//   socket.on('connect_error', (error) => {
+//     console.error('Socket connection error:', error);
+//     console.log('FAILED');
+//   });
+
+//   const addUser = () => {
+//     const data = {
+//       first_name: 'From front end websockets version2',
+//       last_name: 'From front end websockets version2',
+//       email: 'From front end websockets version2@gmail.com',
+//       scripts_id_array: [],
+//     };
+//     socket.emit('add_user', data);
+//   };
+
+//   useEffect(() => {
+//     // Yjs setup for syncing editor content
+//     yText.observe((event) => {
+//       const text = yText.toString();
+//       editorRef.current?.setContentHTML(text);
+//     });
+
+//     return () => {
+//       yText.unobserve(() => {});
+//       socket.disconnect();
+//     };
+//   }, []);
+
+//   const handleEditorChange = (html: string) => {
+//     yText.delete(0, yText.length);
+//     yText.insert(0, html);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Toxic Positivity is for Realzzzzzz</Text>
+//       <Button title="Add User" onPress={addUser} />
+//       <RichEditor
+//         ref={editorRef}
+//         style={styles.editor}
+//         onChange={handleEditorChange}
+//       />
+//       <RichToolbar editor={editorRef} />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   item: {
+//     backgroundColor: '#f9c2ff',
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 24,
+//     color: 'red',
+//   },
+//   body: {
+//     color: 'red',
+//   },
+//   errorText: {
+//     color: 'red',
+//   },
+//   editor: {
+//     flex: 1,
+//     alignSelf: 'stretch',
+//     marginVertical: 20,
+//     backgroundColor: '#f3f3f3',
+//     borderColor: '#ddd',
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     padding: 10,
+//   },
+// });
+
+// export default ScreenwritingEditor;
